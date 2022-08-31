7d3c248dd90e9b58302c6671f94040734d3035c1
superbin <837076716@qq.com>
2022-08-31 19:59:14 +0800

    7-6路由与页面

---
+++ src/router/modules/social.js
@@ -0,0 +1,18 @@
+// 员工的路由规则
+import layout from '@/layout'
+export default {
+  path:"/social",
+  // name:'social',//给模块的一级路由加一个name属性  这个属性我们会在做权限的时候用到
+  component:layout,
+  children:[
+    // 二级路由的path什么都不用写的时候,此时它表示二级路由的默认路由
+    {
+      path:'', //这里不用写,什么都不用写表示 /employees 不但有布局 layout => 员工主页
+      component:() => import('@/views/social'),
+      meta:{
+        title:'社保', //这里为什么要用title,左侧刁航读取了这里的title属性
+        icon:'table'
+      }
+    }
+  ]
+}
